6f59d1c81a5230090b6441f6cda4f1fa497f0bb0
Babak Chehraz <bchehraz@csumb.edu>
2018-06-19 11:48:39 -0700

    Added BgImage component and gatsby-image dependency

---
+++ src/components/BgImage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Img from 'gatsby-image';
+
+const BgImage = (props) => (
+    <Img
+      sizes={props.image.sizes}
+      style={{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        height: '100vh',
+        width: '100%',
+        zIndex: '-1',
+      }}
+    />
+);
+
+export default BgImage;
