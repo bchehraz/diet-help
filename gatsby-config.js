262cec9ec3beac82a457cb5ed5ee58f6ae18743f
Babak Chehraz <bchehraz@csumb.edu>
2018-06-19 13:04:21 -0700

    Added needed gatsby dependencies for BgImage to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Picking the Right Diet',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img/`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 }
