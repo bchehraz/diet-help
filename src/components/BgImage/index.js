08dfbf643dc68441d17274a21cfa2963a0d26e6c
Babak Chehraz <bchehraz@csumb.edu>
2018-06-21 16:44:26 -0700

    Added customizable height and width to BgImage component

--- src/components/BgImage/index.js
+++ src/components/BgImage/index.js
@@ -4,15 +4,27 @@ import Img from 'gatsby-image';
 const BgImage = (props) => (
     <Img
       sizes={props.image.sizes}
-      style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        height: '100vh',
-        width: '100%',
-        zIndex: '-1',
-      }}
+      style={
+        (props.style) ? { ...props.style, ...styles.base } : styles.img
+      }
     />
 );
+
+const styles = {
+  img: {
+    width: '100%',
+    height: '100vh',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    zIndex: '-1',
+  },
+  base: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    zIndex: '-1',
+  }
+}
 
 export default BgImage;
